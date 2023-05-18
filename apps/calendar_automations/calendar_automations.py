232b165278d9985495e0732d451ce6d45e958e9b
max-github-mjhodgson-com <max-github@mjodgson.com>
2023-05-18 12:00:45 +0100

    Made event code move efficent.

--- apps/calendar_automations/calendar_automations.py
+++ apps/calendar_automations/calendar_automations.py
@@ -29,24 +29,13 @@ class Calendar_Automations(hass.Hass):
     self.log(this_script + " running at {}.".format(now))
     self.log("=" * globals.log_partition_line_length)
     
-    # Load external apps:
-    global FunctionLibrary
-    FunctionLibrary = self.get_app("function_library")
-        
-    global GarageLibrary
-    GarageLibrary = self.get_app("garage")
-
-    global HouseModeLibrary
-    HouseModeLibrary = self.get_app("house_mode")
-        
-    global SqueezeboxLibrary
-    SqueezeboxLibrary = self.get_app("squeezebox_control")
-
-    global DoorPhoneLibrary
-    DoorPhoneLibrary = self.get_app("doorphone")
-
-    global CarLibrary
-    CarLibrary = self.get_app("car")
+    # Load external app libraries:
+    #self.function_library = self.get_app("function_library")  # Not currently used.
+    self.garage_library = self.get_app("garage")
+    self.house_mode_library = self.get_app("house_mode")
+    self.squeezebox_library = self.get_app("squeezebox_control")
+    self.door_phone_library = self.get_app("doorphone")
+    self.car_library = self.get_app("car")
     
     # Listen for events:
     self.listen_event(self.on_calendar_event, entity_id = globals.house_calendar)
@@ -93,38 +82,40 @@ class Calendar_Automations(hass.Hass):
 ###############################################################################################################
   def calendar_event_start(self, calendar_event_title):
       self.log("Calendar event started: " + calendar_event_title)
-      calendar_event_title_split = calendar_event_title.split()
+      calendar_event_title_split = calendar_event_title.split().lower()
       self.log("Calendar title: " + str(calendar_event_title_split))
       calendar_event_title_word_count = len(calendar_event_title_split)
-      if calendar_event_title_split[0].lower() == "garage":
-        if calendar_event_title_split[1].lower() =="door":
-          if calendar_event_title_split[2].lower() == "open":
-            self.log("Garage door open.")
-            GarageLibrary.power_on_and_open_garage
-          elif calendar_event_title_split[2].lower() == "close":
-            self.log("Garage door close.")
-            GarageLibrary.close_garage_and_power_off
-        elif calendar_event_title_split[1].lower() =="power" and calendar_event_title_split[2].lower() =="on":
-           self.log("Garage power on.")
-           GarageLibrary.switch_on_garage_door
-        elif calendar_event_title_split[1].lower() =="power" and calendar_event_title_split[2].lower() =="off":
-          self.log("Garage power off.")
-          GarageLibrary.switch_off_garage_door
-      elif calendar_event_title_split[0].lower() == "telegram" and calendar_event_title_split[1].lower() == "message":
-        telegram_message = calendar_event_title_split[2:]
-        self.log("Telegram message: " + " ".join(telegram_message))
-        self.call_service(globals.max_telegram, title = "Calendar Reminder: ", message = " ".join(telegram_message))
-      elif calendar_event_title_split[0].lower() == "ha" and calendar_event_title_split[1].lower() == "message":
-        ha_message = calendar_event_title_split[2:]
-        self.log("HA message: " + " ".join(ha_message))
-        self.call_service(globals.max_app, title = "Calendar Reminder: ", message = " ".join(ha_message))
-      elif calendar_event_title_split[0].lower() == "house":
-        if calendar_event_title_split[1].lower() == "mode":
-          self.log("House Mode.")
-          available_house_modes = ["Out", "Away", "Pre-arrival"]
-          house_mode = calendar_event_title_split[2]
-          if house_mode in available_house_modes:
-            self.select_option(globals.house_mode_selector, house_mode)
+      if calendar_event_title_word_count > 1:
+        command_string = ' '.join(calendar_event_title_split[0:2])
+        match command_string:
+          case "garage door":
+            if calendar_event_title_split[2] == "open":
+              self.log("Garage door open.")
+              self.garage_library.power_on_and_open_garage
+            elif calendar_event_title_split[2] == "close":
+              self.log("Garage door close.")
+              self.garage_library.close_garage_and_power_off
+          case "garage power":
+            if calendar_event_title_split[2] =="on":
+              self.log("Garage power on.")
+              self.garage_library.switch_on_garage_door
+            elif calendar_event_title_split[2] =="off":
+              self.log("Garage power off.")
+              self.garage_library.switch_off_garage_door
+          case "telegram message":
+            telegram_message = calendar_event_title_split[2:]
+            self.log("Telegram message: " + " ".join(telegram_message))
+            self.call_service(globals.max_telegram, title = "Calendar Reminder: ", message = " ".join(telegram_message))
+          case "ha message":
+            ha_message = calendar_event_title_split[2:]
+            self.log("HA message: " + " ".join(ha_message))
+            self.call_service(globals.max_app, title = "Calendar Reminder: ", message = " ".join(ha_message))
+          case "house mode" :
+            self.log("House Mode.")
+            available_house_modes = ["out", "away", "pre-arrival"]
+            house_mode = calendar_event_title_split[2]
+            if house_mode in available_house_modes:
+              self.select_option(globals.house_mode_selector, house_mode)
 
 
   def calendar_event_end(self, calendar_event_title):
@@ -134,10 +125,10 @@ class Calendar_Automations(hass.Hass):
       if calendar_event_title_split[0].lower() == "garage":
         if calendar_event_title_split[1].lower() =="door":
           self.log("Garage close.")
-          GarageLibrary.close_garage_and_power_off
+          self.garage_library.close_garage_and_power_off
         elif calendar_event_title_split[1].lower() =="power" and calendar_event_title_split[2].lower() =="on":
            self.log("Garage power off.")
-           GarageLibrary.switch_off_garage_door
+           self.garage_library.switch_off_garage_door
 
   # Check if event is in progress.
   # Usage:
