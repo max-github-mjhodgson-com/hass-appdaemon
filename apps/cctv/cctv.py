2a0dcc5819d5c47960a9ebcbe23a4f48c99e8191
max-github-mjhodgson-com <max-github@mjodgson.com>
2023-10-30 12:06:41 +0000

    Updates to frigate url and snapshots.

--- apps/cctv/cctv.py
+++ apps/cctv/cctv.py
@@ -27,7 +27,9 @@ class Cctv(hass.Hass):
     self.mqtt = self.get_plugin_api("MQTT")
     
     #Set up some variables:
-    self.frigate_camera_url = "http://" + globals.frigate_hostname + ":" + globals.frigate_port + "/api/"  #+ camera_location +"/latest.jpg"
+    self.frigate_camera_url = "http://" + globals.frigate_hostname + ":" + globals.frigate_port + "/api/"  #+ camera_location +"/latest.jpg
+    #self.get_picture_from_frigate("front_doorbell", "card")
+
     
     #self.set_value(globals.front_motion_detection_off_input_number, 0)
 
@@ -60,14 +62,14 @@ class Cctv(hass.Hass):
     false = 0  # Do not remove.
     null = "null"
     media_base_path = globals.cctv_media_location
-    #video_base_url = "http://" + globals.frigate_hostname + ":" + globals.frigate_port + "/api/events/"
     video_base_url = "https://" + globals.frigate_external_hostname + "/api/events/"
-    picture_url = "http://" + globals.home_assistant_url + ":8123/api/frigate/notifications/"
+    picture_url = "https://" + globals.home_assistant_url + "/api/frigate/notifications/"
     payload = eval(data['payload'])
     #event_camera = data['topic']['before']['camera']
     #event_label = data['topic']['before']['label']
     self.log("Payload: " + str(payload))
     event_id_before = payload['before']['id']
+    event_camera_before = payload['before']['camera']
     event_id_after = payload['after']['id']
     event_label_before = payload['before']['label']
     event_label_after = payload['after']['label']
@@ -75,10 +77,13 @@ class Cctv(hass.Hass):
     event_number_of_entered_zones_before = len(event_entered_zones_before)
     event_entered_zones_after = payload['after']['entered_zones']
     event_type = payload['type']
+    self.log("Camera ID: " + str(event_camera_before))
     self.log("Before ID: " + event_id_before)
     self.log("After ID: " + event_id_after)
     self.log("Event Type: " + event_type)
     self.log("Object Type: " + str(event_label_after))
+    # Create a sensor in Home Assistant recording the last event ID:
+    self.set_state("sensor.cctv_front_doorbell_last_event_id", state = event_id_before, attributes = {"friendly_name": "Front Doorbell Last Event ID"})
     if event_number_of_entered_zones_before > 0 and event_type != "":
       self.log("Entered Zones: " + str(event_entered_zones_before))
       self.send_an_alert(zone = event_entered_zones_before, detected_object = event_label_after)
@@ -86,18 +91,30 @@ class Cctv(hass.Hass):
       self.log("No zones were entered.")
     if event_label_before == "person":
       if event_type == "new":
+        self.log("Number of persons in zone: " + str(self.get_state(globals.persons_in_zone_count[event_camera_before])))
+        #self.call_service("device_tracker/see", dev_id = globals.max_phone_tracker)
+        #for i in range(len(globals.mobile_app_users)):
+        #  self.call_service(globals.mobile_app_users[i], message = "request_location_update")
+        self.call_service(globals.max_app, message = "request_location_update")
         self.log("Sending start image.(New Version, via telegram.)")
         picture_url_begin = picture_url + event_id_before + "/snapshot.jpg"
         self.log("Image URL: " + picture_url_begin)
         if self.get_state(globals.front_doorbell_person_detection_switch) == "on":
-          self.call_service("telegram_bot/send_photo", url = picture_url_begin, caption = "Person at front (New Version).")
+          self.log("Detection switched on sending image.")
+          self.call_service("telegram_bot/send_photo", url = picture_url_begin, caption = "Person at front (New Version CCTV).")
           self.call_service(globals.max_app, title = "Doorphone Alert (New)",\
                                              message = "Person at Front Door",\
                                              data = {"channel":"Front_Door",\
                                                      "clickAction":globals.lovelace_cctv_tab ,\
                                                      "image": picture_url_begin})
-          self.call_service("device_tracker/see", dev_id = globals.max_phone_tracker)
+          self.call_service(globals.hall_panel_app, title = "Person detected at the front.",\
+                                             message = "TTS",\
+                                             data = {"media_stream": "alarm_stream",\
+                                                     "tts_text": "Person detected at the front.",\
+                                                     "timeout": 60 })
+
       if event_type == "end":
+        self.log("Entered zones after: " + str(event_entered_zones_after))
         self.log("Sending end image (via telegram).")
         picture_url_end = picture_url + event_id_after + "/snapshot.jpg"
         self.log("picture_url_end: " + str(picture_url_end))
@@ -107,6 +124,7 @@ class Cctv(hass.Hass):
         front_door_detection_switch_status = self.get_state(globals.front_doorbell_person_detection_switch)
         if front_door_detection_switch_status == "on":
           self.log("Detection switch status (end image): " + str(front_door_detection_switch_status))
+
           self.call_service("telegram_bot/send_photo", disable_notification = "yes", url = picture_url_end, caption = video_url)
           #self.call_service("telegram_bot/send_video", disable_notification = "yes", timeout = "yes", url = video_url, caption = "Person at front (video clip).")
           self.call_service(globals.max_app, title = "Doorphone Alert (New) end image",\
@@ -114,6 +132,7 @@ class Cctv(hass.Hass):
                                              data = {"channel":"Front_Door",\
                                                      "clickAction":globals.lovelace_cctv_tab ,\
                                                      "image": picture_url_end})
+
     # + event_camera + event_label)
     #event_type = data['topic']['after']['type']
     #self.log(event_type)
@@ -139,16 +158,20 @@ class Cctv(hass.Hass):
  # Take a picture.
   def get_picture_from_frigate(self, camera_id, picture_type):   #, picture_caption):
     #self.log("Take picture: " + picture_type)
-    snapshot_filename = globals.cctv_media_location + "/" + camera_id + "/latest/" + camera_id + "_" + picture_type + ".jpg"
+    snapshot_filename = globals.cctv_media_location + "/" + camera_id + "/latest/" + picture_type + ".jpg"
     image_timestamp = datetime.strftime(self.datetime(), '%Y%m%d_%H%M%S')
     directory_datestamp = datetime.strftime(self.datetime(), '%Y/%b/%d-%a')
-    picture_filename =  camera_id + "_" + picture_type + "." + image_timestamp + ".jpg"
+    picture_filename =  picture_type + "." + image_timestamp + ".jpg"
     picture_directory = globals.cctv_media_location + "/" + camera_id +"/" + directory_datestamp
-    os.makedirs(picture_directory)
+    self.log("Create directory: " + str(picture_directory))
+    try:
+      os.makedirs(picture_directory)
+    except Exception:
+      self.log("Error creating directory, probably already exists.")
     #timed_snapshot_filename = globals.cctv_media_location + "/" + camera_id +"/" + directory_datestamp + "/" + camera_id + "_" + picture_type + "." + image_timestamp + ".jpg"
     timed_snapshot_filename = picture_directory + "/" + picture_filename
-    self.get_latest_camera_picture(image_url = self.frigate_camera_url + camera_id + "/latest.jpg", image_filename = timed_snapshot_filename)
-    self.get_latest_camera_picture(image_url = self.frigate_camera_url + camera_id + "/latest.jpg", image_filename = snapshot_filename)
+    self.get_latest_camera_picture(image_url = "https://" + globals.frigate_external_hostname + "/api/" + camera_id + "/latest.jpg", image_filename = timed_snapshot_filename)
+    self.get_latest_camera_picture(image_url = "https://" + globals.frigate_external_hostname + "/api/" + camera_id + "/latest.jpg", image_filename = snapshot_filename)
     return timed_snapshot_filename, snapshot_filename
 
   def get_latest_camera_picture(self, image_url, image_filename):
@@ -156,6 +179,7 @@ class Cctv(hass.Hass):
     img_data = requests.get(image_url).content
     with open(image_filename, 'wb') as handler:
       handler.write(img_data)
+      handler.close()
 
   def send_an_alert(self, **kwargs):
     #self.log("Send an alert.")
